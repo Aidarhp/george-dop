5c6cf1495088877871508df127f21e7488c9c102
Aidar satubaldiev <aydatsatubaldiev@gmail.com>
2022-11-23 20:01:53 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import George from "./components/george/George";
+import './scss/style.scss'
+
+function App() {
+  return (
+    <>
+    <George/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/george/George.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const George = () => {
+  return (
+    <section className='george'>
+      <div className='container'>
+            <div className='george__block'>
+                <h2 className='george__title'><span className='george__title-first'>бренд</span><br/>george gina & lucy</h2>
+                <p className='george__p'>Бренд основан в 2004 году недалеко от Франкфурта, когда супруги Николь Бейли и Николас Нойхаус вместе с Оливером Брюном выпустили свою первую и очень удачную коллекцию сумок, каждая из которых имела свое имя. </p>
+            </div>
+      </div>
+      <div className='back__george'></div>
+    </section>
+  )
+}
+
+export default George
